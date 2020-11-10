fc626db397a5272fb528e1f0d5e6bca1cc79e44e
Pranal PradeepKumar Kuttappan <pranalpk8@gmail.com>
2020-11-09 21:27:58 -0500

    API and logic implementation

---
+++ src/components/QuizCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {AnswerObject} from '../App';
+//import {Wrapper, ButtonWrapper} from './QuestionCard.styles';
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const QuizCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNum} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}/>
+      <div>
+        {answers.map((answer) => (
+          <div key={answer}>
+            <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuizCard;
